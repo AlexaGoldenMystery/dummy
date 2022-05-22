a62c83fe6d29af086e6767fe78a3d69ec8f687da
AlexaGoldenMystery <alexluisa.bu@gmail.com>
2022-05-22 19:56:05 +0200

    Update smallwindow.py

--- smallwindow.py
+++ smallwindow.py
@@ -7,8 +7,7 @@ def main():
     app = QApplication(sys.argv)         #new application is created
 
     w = QWidget()                       #new window is created
-    w.resize(250, 150)                  # size of the window
-    w.move(300, 300)                    # window starts at 300, 300px
+    w.resize(250, 150)                  #300px
     w.setWindowTitle('Simple')          # window title
     w.show()                            #window is going to show
 
